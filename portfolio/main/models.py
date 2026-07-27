cd993c9ddaec9dbdd14832d31f22b6a85e2aa1b3
MrOrayoh <odhiambobran004@gmail.com>
2026-07-27 10:49:43 +0300

    2nd commit

---
+++ portfolio/main/models.py
@@ -0,0 +1,8 @@
+# Create your models here.
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    link = models.URLField()
